c6a0778fdf1eb5a07e923b38a973ec45ad84d356
muhammedfariis <7591faris@gmail.com>
2026-01-06 17:03:11 +0530

    updated

--- src/detector.js
+++ src/detector.js
@@ -1,26 +1,32 @@
-import { Hands } from '@mediapipe/hands';
-import { Camera } from '@mediapipe/camera_utils';
+import { Hands } from "@mediapipe/hands";
+import { Camera } from "@mediapipe/camera_utils";
 
 export class HandDetector {
   constructor() {
-    this.video = document.getElementById('webcam');
-    this.hands = new Hands({
-      locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`
+    this.video = document.getElementById("webcam");
+    // src/detector.js update
+     this.hands = new Hands({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${Hands.VERSION}/${file}`;
+      },
     });
 
     this.hands.setOptions({
       maxNumHands: 1,
       modelComplexity: 1,
       minDetectionConfidence: 0.6,
-      minTrackingConfidence: 0.6
+      minTrackingConfidence: 0.6,
     });
   }
 
   async init(onResults) {
     this.hands.onResults(onResults);
     const camera = new Camera(this.video, {
-      onFrame: async () => { await this.hands.send({ image: this.video }); },
-      width: 640, height: 480
+      onFrame: async () => {
+        await this.hands.send({ image: this.video });
+      },
+      width: 640,
+      height: 480,
     });
     return camera.start();
   }
